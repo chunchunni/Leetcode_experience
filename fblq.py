5904c0fa2b94232111725574ed5d78671e7025c6
chunchunni <1808790564@qq.com>
2021-04-07 16:43:15 +0800

    0

--- fblq.py
+++ fblq.py
@@ -1,5 +1,5 @@
 from typing import List
-def fb(n: int) -> int:
+def fb1(n: int) -> int: #解决方法一
     memo = [0]*(n+1)
 
     def fb_remember(memo: List[int], n: int)-> int:
@@ -12,5 +12,19 @@ def fb(n: int) -> int:
 
     return fb_remember(memo,n)
 
-print(fb(7))
+print(fb1(10))
+
+def fb2(n: int) -> int:#解决方法二
+    if n == 0 or n == 1:
+        return n
+    dp_0 = 0
+    dp_1 = 1
+    for i in range(2,n+1):
+        temp = dp_1
+        dp_1 = dp_0 + dp_1
+        dp_0 = temp
+    return dp_1
+
+print(fb2(10))
+
 
